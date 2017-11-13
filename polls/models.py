117b8723bde9316bc1498a87f1569f1433f05fe4
sirajul.anik <sirajul.anik@pathao.com>
2017-11-13 18:19:03 +0600

    migration added for polls

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Question(models.Model):
+    db_table = 'questions'
+    question_text = models.CharField(max_length=200, verbose_name='Question')
+    publication_date = models.DateTimeField('Publish at: ', name='published_at')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Choice(models.Model):
+    class Meta:
+        db_table = 'choices'
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200, verbose_name='choice')
+    votes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
