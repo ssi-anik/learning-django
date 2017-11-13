267257464945899b95ac8bc666eb911159ea910b
sirajul.anik <sirajul.anik@pathao.com>
2017-11-13 19:51:05 +0600

    added __str__ to classes

--- polls/models.py
+++ polls/models.py
@@ -9,6 +9,9 @@ class Question(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     class Meta:
@@ -19,3 +22,6 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.choice_text
